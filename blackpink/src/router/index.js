765d1fc66ced8a6dfec06bd4b467115e0fb92be7
John Huynh <duyhuynh55110@gmail.com>
2020-09-17 23:14:40 +0700

    nested router

--- blackpink/src/router/index.js
+++ blackpink/src/router/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/Home.vue'
 
+// Posts
+import PostSingle from "../views/Posts/Single.vue"
+import Posts from '../views/Posts/index.vue'
+
 Vue.use(Router)
 
 export default new Router({
@@ -19,6 +23,18 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+      path: '/posts',
+      name: 'Posts',
+      component: Posts,
+      children: [
+        {
+          path: ":id",
+          name: "PostSingle",
+          component: PostSingle,
+        },
+      ]
     }
   ]
 })

25af75ccd6f56b46b5a80f9e2f395014a7ffec69
Duy Huynh <duyhuynh55110@gmail.com>
2021-02-06 01:12:34 +0700

    update code

--- blackpink/src/modules/Posts/containers/Archive.vue
+++ blackpink/src/modules/Posts/containers/Archive.vue
@@ -2,17 +2,15 @@
   <div class="post-archive">
     <section class="archive-area section_padding_80">
       <div class="container">
-        <PostsArea />
+        
       </div>
     </section>
   </div>
 </template>
 <script>
-import PostsArea from "@posts/components/PostsArea";
 export default {
   name: "PostArchive",
   components: {
-    PostsArea,
   },
 };
 </script>
